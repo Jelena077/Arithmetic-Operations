973a198773c691cece4fc0a24c16fac076258526
Jelena077 <jelenaasavicc@googlemail.com>
2023-08-20 21:42:03 +0200

    Add project files.

---
+++ Arithmetic Operations/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetic_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result;
+            int a = 10, b = 5;
+            result = a + b;
+            Console.WriteLine("Sum: " + result);
+            result = a - b;
+            Console.WriteLine("Difference: " + result);
+            result = a * b;
+            Console.WriteLine("Product: " + result);
+            result = a / b;
+            Console.WriteLine("Quotient: " + result);
+            Console.ReadLine();
+        }
+    }
+}
